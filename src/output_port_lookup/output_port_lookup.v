651dd212c9fd5d0f2d85b2ded03b5eff78107fdf
warper321 <warper321@sbcglobal.net>
2011-03-02 11:06:55 -0800

    switch doc report

--- src/output_port_lookup/output_port_lookup.v
+++ src/output_port_lookup/output_port_lookup.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /////////////////////////////////////////////////////////////////////////////
 // vim:set shiftwidth=3 softtabstop=3 expandtab:
 // $Id: output_port_lookup.v 5240 2009-03-14 01:50:42Z grg $
@@ -57,43 +56,49 @@
          while(2**log2<number) begin
             log2=log2+1;
          end
-	 
+
       end
-      
+
    endfunction
-   
+
    //--------------------- Internal Parameter-------------------------
    localparam IN_MODULE_HDRS	  = 4'd0;
-   localparam IN_ETHERNET_HDRS    = 4'd1;
-   localparam IN_PACKET           = 4'd2;
-   
+   localparam DST_ADDR		  = 4'd1;
+   localparam SRC_ADDR            = 4'd2;
+   localparam CHECK_SRC_CAM	  = 4'd3;
+   localparam CHECK_DST_CAM	  = 4'd4;
+   localparam CHECK_RAM		  = 4'd5;
+   localparam STORE_CAM		  = 4'd6;
+   localparam STORE_RAM		  = 4'd7;
+   localparam IN_PACKET		  = 4'd8;
+
+
    //---------------------- Wires/Regs -------------------------------
    reg [DATA_WIDTH-1:0] in_data_modded;
-   
-   reg [15:0] 		decoded_src;
+
+   reg [15:0] 		decoded_src, src_addr_1;
+   reg [15:0]       port_srcaddr, port_dstaddr;
+   reg [15:0]       hits, misses;
    wire [DATA_WIDTH-1:0] out_data_modded;
    wire [CTRL_WIDTH-1:0] out_ctrl_modded;
- 
-   reg [15:0]       decoded_dst;
-   reg              state, state_nxt;
-   reg [47:0]		source_addr;
+   reg [3:0]            state, state_nxt;
+   reg                  read_cam_dst, read_cam_dst2, read_cam_src;
+   reg [47:0]		src_addr;
    reg [47:0]		dst_addr;
    reg [63:0]		pack1;
-   reg [63:0]		pack2;
-   reg [1:0]		count;
+   reg [3:0]            count_cam = 4'b0;
+   reg [3:0]		count = 0;
    reg [15:0]		src_port;
-   reg [47:0]		cam_in;
-   reg 			cam_we, ram_we, send_pkt;
-   reg [3:0]		cam_addr, ram_addr, cam_index, ram_index;
-   reg [15:0]		ram_in, ram_port;
+   reg [47:0]		cam_in, cam_cmp;
+   reg 			cam_we, cam_en, ram_we;
+   reg                  send_pkt, new_pkt;
+   reg [3:0]		cam_addr, ram_addr, cam_index, dst_cam_index;
+   reg                  ram_index = 1'b0;
+   reg [55:0]		ram_in;
    wire                 cam_match;
-   wire [15:0] 	        ram_out;
+   wire [55:0] 	        ram_out;
    wire                 cam_busy;
    wire [3:0] 		cam_out;
-		
-   
-
- 		
 
    //----------------------- Modules ---------------------------------
    small_fifo #(.WIDTH(CTRL_WIDTH+DATA_WIDTH), .MAX_DEPTH_BITS(2))
@@ -109,10 +114,24 @@
          .reset         (reset),
          .clk           (clk)
          );
-	//-----------------------CAM for MAC Address---------------------
-	cam_mac mac (clk, cam_in, cam_we, cam_addr, cam_busy, cam_match, cam_out);
-	//-----------------------RAM for PORT Address--------------------
-	ram_port port (clk, ram_in, ram_addr, ram_we, ram_out);
+   //-----------------------CAM for MAC Address---------------------
+    cam_mac mac(
+	.clk		(clk),
+	.cmp_din	(cam_cmp),	//Bus [47:0]
+	.din		(cam_in), 	//Bus [47:0]
+	.en		(cam_en),
+	.we		(cam_we),
+	.wr_addr	(cam_addr),	//Bus [3:0]
+	.busy		(cam_busy),
+	.match		(cam_match),
+	.match_addr	(cam_out));	//Bus [3:0]
+   //-----------------------RAM for PORT Address--------------------
+	ram_mem port (
+	.clka		(clk),
+	.dina		(ram_in),	// Bus [55:0]
+	.addra		(ram_addr),	// Bus [3:0]
+	.wea		(ram_we),	// Bus[0:0]
+	.douta		(ram_out));	// Bus[55:0]
    //----------------------- Logic ---------------------------------
 
    assign in_rdy = !in_fifo_nearly_full;
@@ -120,118 +139,149 @@
    /* pkt is from the cpu if it comes in on an odd numbered port */
    assign pkt_is_from_cpu = in_data[`IOQ_SRC_PORT_POS];
 
-  
    /* modify the IOQ module header */
    always @(*) begin
 
-    in_data_modded   = in_data;
-    state_nxt        = state;
-	decoded_src = 0;
+	in_data_modded   = in_data;
+//	state_nxt        = state;  can't change states twice, and not on a clock
+	decoded_src	 = 0;
 	decoded_src[in_data[`IOQ_SRC_PORT_POS+15:`IOQ_SRC_PORT_POS]] = 1'b1;
-	src_port = in_data[`IOQ_SRC_PORT_POS+15:`IOQ_SRC_PORT_POS];
-      
-      case (state)
-         IN_MODULE_HDRS: begin
-			count = 2'b00;
-	    
-            if(in_wr && in_ctrl==IO_QUEUE_STAGE_NUM) begin
-	       
-               if(pkt_is_from_cpu) begin
-		ram_port = ~decoded_src & 16'b0000000001010101;
-                in_data_modded[`IOQ_DST_PORT_POS+15:`IOQ_DST_PORT_POS] = ram_port;
-               end
-	       
-            end
-	    
-            if(in_wr && in_ctrl==0) begin
-               state_nxt = IN_ETHERNET_HDRS;
-            end
-	    
-         end // case: IN_MODULE_HDRS
-         
-         IN_ETHERNET_HDRS: begin
-	    	    
-            if(in_wr) begin
-	       if(count == 2'b00) begin
-			count = 2'b01;
-	       end
-	       	       
-	       else if(count == 2'b01) begin
-		    pack1 = in_data;
-		  dst_addr = pack1[63:16];
-		  count = 2'b10;
-	       end
-	       
-	              	       
-	      else if(count == 2'b10) begin
-		    pack2 = in_data;
-		  source_addr = {pack1[15:0], pack2[63:32]};
-		   count = 2'b00;
-		    state_nxt = IN_PACKET;
-	      end
-	       
-	       cam_in = source_addr;
-	       
-			if(cam_match) begin
-			   	cam_index = cam_out;
-				ram_index = cam_index;
-				ram_index = ram_addr;
-				send_pkt = 1;
-				ram_port = ram_out;
+
+    case (state)
+		IN_MODULE_HDRS: begin
+
+        	if(in_wr && in_ctrl==IO_QUEUE_STAGE_NUM) begin
+            	read_cam_dst = 1'b0;
+	       		send_pkt = 0;
+               	if(pkt_is_from_cpu) begin
+			  		port_srcaddr = 16'b0;
+				  	in_data_modded[`IOQ_DST_PORT_POS+15:`IOQ_DST_PORT_POS] = port_srcaddr;
+		       	end
+
+				else
+              	 	port_srcaddr = decoded_src;
+        		state_nxt = DST_ADDR;
+	    	end // if (in_wr && in_ctrl==IO_QUEUE_STAGE_NUM)
+        end // case: IN_MODULE_HDRS
+
+        DST_ADDR:  begin
+
+			if(in_wr && in_ctrl == 0 ) begin
+	    		pack1 = in_data;
+		  		dst_addr = pack1[63:16];
+		  		src_addr_1 = pack1[15:0];
+            	read_cam_dst = 1;
+		  		state_nxt = SRC_ADDR;
+	    	end
+	 	end // case: DST_ADDR
+
+        SRC_ADDR: begin
+
+	   		src_addr = {src_addr_1, in_data[63:32]};
+			//cam_in = src_addr;
+		  	cam_en = 1;
+		  	state_nxt = CHECK_SRC_CAM;
+	  	end // case: SRC_ADDR
+
+		CHECK_SRC_CAM: begin
+
+			cam_cmp = src_addr;
+			if(!cam_busy) begin
+				if(cam_match) begin
+					cam_index = cam_out;
+	//				cam_en = 0;
+					ram_we = 1;
+					state_nxt = STORE_RAM;
+				end
+				else begin
+					cam_we = 1;
+					state_nxt = STORE_CAM;
+				end
 			end
-	              
-			else begin
-				cam_in = source_addr;
-				ram_addr = cam_out;
-				ram_in = ram_port; 
+		end // case: CHECK_SRC_CAM
+
+		CHECK_DST_CAM: begin
+
+			cam_cmp = dst_addr;
+			if(!cam_busy) begin
+				if(cam_match) begin
+					ram_index = cam_out;
+					cam_en = 0;
+					state_nxt = CHECK_RAM;
+				end
+				else begin
+					port_dstaddr = ~port_srcaddr & 16'h0055;
+					send_pkt = 1;
+					state_nxt = IN_PACKET;
+				end
+			end
+		end // case: CHECK_DST_CAM
+
+		CHECK_RAM: begin
+         	ram_index = ram_addr;
+			if(ram_index == ram_addr) begin
+				port_dstaddr = ram_out[55:48];
 				send_pkt = 1;
-			end // else: !if(cam_match)
-	       
-	    end // if (in_wr)
-	 end // case: IN_ETHERNET_HDRS
-	      
-          		 
-                 IN_PACKET: begin
-		    if(in_wr && in_ctrl!=0)begin
-		       
-		      state_nxt = IN_MODULE_HDRS;
-		    end
-		    
-		 end
-      
-        endcase // case(state)
-   end // always @ (*)
-
-   always @(posedge clk) begin
-      if(reset) begin
-         state <= IN_MODULE_HDRS;
-      end
-      
-      else begin
-         state <= state_nxt;
-      end
-   end
-
-   /* handle outputs */
-   assign in_fifo_rd_en = out_rdy && !in_fifo_empty && send_pkt;
-   always @(posedge clk) begin
-    out_wr <= reset ? 0 : in_fifo_rd_en;
-	if(out_ctrl_modded != IO_QUEUE_STAGE_NUM) begin
-		out_data <= out_data_modded;
-		out_ctrl <= out_ctrl_modded;
-	end
-      
-		else begin
-		out_ctrl <= out_ctrl_modded;
-		out_data <= {ram_port, out_data_modded[47:0]};
+			end
+			state_nxt = IN_PACKET;
 		end
-      
-   end // always @ (posedge clk)
-   
+
+		STORE_CAM: begin
+			cam_addr = count;
+			cam_in = src_addr;
+         		cam_we = 0;
+				ram_we = 1;
+			state_nxt = STORE_RAM;
+		end // case: STORE_CAM
+
+		STORE_RAM: begin
+			ram_addr = count;
+			ram_in[55:0] = {port_srcaddr,src_addr};
+			count = count+1;
+			state_nxt = CHECK_DST_CAM;
+		end
+
+       	IN_PACKET:  begin
+
+			if(in_wr && in_ctrl!=0)begin
+			  	state_nxt = IN_MODULE_HDRS;
+			end
+		end
+
+    endcase // case (state)
+	end // always @ (*)
+	   always @(posedge clk) begin
+		if(reset) begin
+			state <= IN_MODULE_HDRS;
+			ram_we <= 0;
+			cam_we <= 0;
+			cam_en <= 0;
+			hits   <= 0;
+			misses <= 0;
+		end
+		else begin
+			state <= state_nxt;
+		end
+	end
+
+	/* handle outputs */
+	assign in_fifo_rd_en = (out_rdy && send_pkt && !in_fifo_empty);
+	always @(posedge clk) begin
+		out_wr <= reset ? 0 : in_fifo_rd_en;
+		if(out_ctrl_modded != IO_QUEUE_STAGE_NUM) begin
+			out_data <= out_data_modded;
+		  	out_ctrl <= out_ctrl_modded;
+	  	end
+	  	else begin
+		  	out_ctrl <= out_ctrl_modded;
+		  	out_data <= {port_dstaddr, out_data_modded[47:0]};
+	  	end
+	end // always @ (posedge clk)
+
 
    /* registers unused */
    always @(posedge clk) begin
-      
+
       reg_req_out        <= reg_req_in;
       reg_ack_out        <= reg_ack_in;
       reg_rd_wr_L_out    <= reg_rd_wr_L_in;
@@ -239,6 +289,6 @@
       reg_data_out       <= reg_data_in;
       reg_src_out        <= reg_src_in;
    end
-     
+
 endmodule // output_port_lookup
 
--- sw/switch_control.pl
+++ sw/switch_control.pl
@@ -31,24 +31,31 @@ elsif ($options{l})
 
 else 
 {
+	print "Mike's ethernet learning switch high level software control script\n";
+	print "only one flag at at time! declare flag with -<option> <arguements> \n";
+	print "options:\n";
+	print "d: display forwarding table\n";
+	print "r: display switch registers\n";
+	print "c: clear forwarding table\n";
+	print "l: add custom forwarding table entries. use like such -l <MAC>:<port>\n";
 }
 
 sub display_forward
 {
-	$table = `regread x y `;
+	my($table) = `regread x y `;
 	print $table;
 }
 
 sub display_registers
 {
-	$register = `regread x y`;
+	my($register) = `regread x y`;
 	print $register;
 }
 
 sub clear_forward
 {
-	$forward = `regwrite x y`;
-	print "forwarding tables cleard\n";
+	my($forward) = `regwrite x y`;
+	print "forwarding tables cleared\n";
 }
 
 sub load_forward
